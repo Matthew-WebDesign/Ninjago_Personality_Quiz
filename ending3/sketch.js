56d6219e8593ef138dfd4bf24650b25b160bf7b0
Matthew-WebDesign <157499816+Matthew-WebDesign@users.noreply.github.com>
2024-12-04 13:19:02 -0500

    Add files via upload

---
+++ ending3/sketch.js
@@ -0,0 +1,26 @@
+let a = 400;
+
+let speed = 2;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(254, 213, 49);
+  fill(0, 0, 0);
+  ellipse(100, a, 100, 100);
+  ellipse(290, a, 100, 100);
+  a = a - speed;
+   if (a < 190 && speed == 2)
+    {
+      a = 400;
+    }
+  
+    console.log(a);
+  
+    rect(1, 1, 399, 120);
+    rect(1, 250, 399, 150);
+   
+  
+}
